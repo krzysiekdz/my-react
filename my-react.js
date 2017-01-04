c7cf9a41562f49e443e0b33d38dfda13ce56aa76
Krzysztof Dziezyk <krzysztof.dziezyk@gmail.com>
2017-01-04 13:54:52 +0100

    children

--- my-react.js
+++ my-react.js
@@ -1,21 +1,29 @@
 
 
-function createVElement(tag, config) {
+function createVElement(tag, config, children = null) {
 	const {className} = config;
 
 	return {
 		tag: tag, 
 		className,
 		dom: null,
+		props: {
+			children,
+		},
 	};
 }
 
 function mountVElement(vElement, parentDOMNode) {
-	const {tag, className} = vElement;
+	const {tag, className, props} = vElement;
 	const domNode = document.createElement(tag);
 	vElement.dom = domNode;
 	if(className !== undefined) {
 		domNode.className = className;
+	}
+	if(props.children) {
+		props.children.forEach(function(child) {
+			mountVElement(child, domNode);
+		});
 	}
 	parentDOMNode.appendChild(domNode);
 }
@@ -25,7 +33,10 @@ function mountVElement(vElement, parentDOMNode) {
 //------------------- test app
 (function testApp() {
 	const parent = document.querySelector('#app');
-	const app = createVElement('div', {className: 'my-class'});
+	const app = createVElement('div', {className: 'my-class'}, [
+		createVElement('h2', {className: 'my-h2'}),
+		createVElement('p', {className: 'my-p'}),
+		]);
 	mountVElement(app, parent);
 })();
 
