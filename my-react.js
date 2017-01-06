dc07bcab04989970250c942dd68a808815fb189a
Krzysztof Dziezyk <krzysztof.dziezyk@gmail.com>
2017-01-06 21:25:56 +0100

    add components

--- my-react.js
+++ my-react.js
@@ -14,6 +14,22 @@ function createVElement(tag, config, children = null) {
 	};
 }
 
+function createVComponent(tag, props) {
+	return {
+		tag,
+		props,
+		dom: null,
+	};
+}
+
+function createElement(tag, config, children) {
+	if(typeof tag == 'function') {
+		return createVComponent(tag, config);
+	} else {
+		return createVElement(tag, config, children);
+	}
+}
+
 function mountVElement(vElement, parentDOMNode) {
 	const {tag, className, style, props} = vElement;
 	const domNode = document.createElement(tag);
@@ -37,32 +53,74 @@ function mountVElement(vElement, parentDOMNode) {
 		
 	}
 	parentDOMNode.appendChild(domNode);
+	return domNode;
 }
 
 function mountVText(vText, parentDOMNode) {
 	parentDOMNode.textContent = vText;
+	return parentDOMNode;
 }
+
+//komponent ma metode render, wirtualny element nie ma render
+function mountVComponent(vComponent, parentDOMNode) {
+	const {tag, props} = vComponent;
+
+	const Component = tag;
+	const instance = new Component(props);
+	const currentElement = instance.render();
+	const dom = mount(currentElement, parentDOMNode);
+	vComponent._instance = instance;
+	vComponent.dom = dom;
+	parentDOMNode.appendChild(dom);
+
+	return dom;
+}	
 
 function mount(input, parentDOMNode) {
 	if(typeof input === 'string' || typeof input === 'number') {
-		mountVText(input, parentDOMNode);
-	} else {
-		mountVElement(input, parentDOMNode);
+		return mountVText(input, parentDOMNode);
+	} else if (typeof input.tag === 'function'){
+		return mountVComponent(input, parentDOMNode);
+	} else if (typeof input.tag === 'string') {
+		return mountVElement(input, parentDOMNode);
 	}
+}
+
+
+class Component {
+	constructor(props) {
+		this.props = props || {};
+	}
+	setState(partialNewState) {
+
+	}
+	render() {}
 }
 
 
 
 //------------------- test app
+
+class App extends Component {
+	render() {
+		return createElement('div', {style: {backgroundColor: 'blue', height: '100px'}}, 
+			createElement('h1', {}, this.props.message));
+	}
+}
+
 (function testApp() {
 	const parent = document.querySelector('#app');
-	const app = createVElement('div', {className: 'my-class', style: {backgroundColor: 'green', height: '100px'}}, [
-		createVElement('h2', {className: 'my-h2', style: {color: 'black'}}, 'react header'),
-		createVElement('p', {className: 'my-p'}, 'react paragraph'),
-		]);
-	mountVElement(app, parent);
+	// const app = createVElement('div', {className: 'my-class', style: {backgroundColor: 'green', height: '100px'}}, [
+	// 	createVElement('h2', {className: 'my-h2', style: {color: 'black'}}, 'react header'),
+	// 	createVElement('p', {className: 'my-p'}, 'react paragraph'),
+	// 	]);
+	// mountVElement(app, parent);
 
 	// app.dom.style = 'background-color: red';
-	console.log(app.dom);
+	// console.log(app.dom);
+
+	mount( createElement(App, {message: 'hello world!'}) ,   parent);
 })();
+
+
 
